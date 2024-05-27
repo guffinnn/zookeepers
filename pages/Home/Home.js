cf37accf2ebf0cc0fcbe646b6ae8b4464c7d98a5
Danik <dashkevich_daniil2006@mail.ru>
2024-05-27 16:34:40 +0300

    feat: add home page with adaptive

---
+++ pages/Home/Home.js
@@ -0,0 +1,22 @@
+const LIST_ROWS = [
+    "Главная",
+    "Животные",
+    "Рабочее время",
+    "Мед. обследования",
+    "Кормление",
+    "Уборка",
+    "Сотрудники"
+];
+
+window.addEventListener('load', () => {
+    const asideUlList = document.querySelector('.aside__ul__list');
+
+    LIST_ROWS.forEach(item => {
+        const listItem = document.createElement('li');
+        const cardsContainer = document.querySelector('.cards__fluid');
+
+        listItem.innerHTML = `<div class="content ${item === "Главная" && 'active'}">${item}</div>`;
+        listItem.classList.add('list__point');
+        asideUlList.appendChild(listItem);
+    });
+});
