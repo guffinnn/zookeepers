d97170f70ac32df9e20e6d261a76fbe1073487c7
Danik <dashkevich_daniil2006@mail.ru>
2024-05-29 12:56:16 +0300

    feat: completed animals page adaptive, add links to pages

--- pages/Animals/Animals.js
+++ pages/Animals/Animals.js
@@ -8,15 +8,86 @@ const LIST_ROWS = [
     "Сотрудники"
 ];
 
+const TABLE_COLUMNS = [
+    "Кличка",
+    "Вид",
+    "Дата рождения",
+    "Дата зачисления",
+    "Мед. показатели",
+    "Фото",
+    "Действия"
+];
+
+const ANIMALS = [
+    {
+        nickname: "Лиля",
+        species: "Собака",
+        date_of_birth: "01.02.1945",
+        date_of_registration: "01.09.1980",
+        medical_indicators: "Не привит",
+        photo: "img.png",
+        actions: ''
+    },
+    {
+        nickname: "Манки",
+        species: "Обезьяна",
+        date_of_birth: "10.01.2020",
+        date_of_registration: "29.05.2024",
+        medical_indicators: "Привит",
+        photo: "img.png",
+        actions: ''
+    }
+];
+
 window.addEventListener('load', () => {
     const asideUlList = document.querySelector('.aside__ul__list');
-
     LIST_ROWS.forEach(item => {
         const listItem = document.createElement('li');
-        const cardsContainer = document.querySelector('.cards__fluid');
 
         listItem.innerHTML = `<div class="content ${item === "Животные" && 'active'}">${item}</div>`;
         listItem.classList.add('list__point');
         asideUlList.appendChild(listItem);
     });
+
+    const tableHead = document.querySelector('.thead tr');
+    TABLE_COLUMNS.forEach(item => {
+        const tableCell = document.createElement('th');
+
+        tableCell.innerHTML =
+            `<p class="head__cell__content 
+                ${item === "Вид" && 'select' ||
+                  (item === "Фото" || item === "Действия") && "numeric"
+                }">
+            ${item}
+            </p>`;
+        tableCell.classList.add('head__cell');
+        tableHead.appendChild(tableCell);
+    });
+
+    const tableContent = document.querySelector('.tbody');
+    let count = 0;
+    while(count < 5) {
+        ANIMALS.forEach(item => {
+            const tableRow = document.createElement('tr');
+
+            for(let field in item) {
+                const tableCell = document.createElement('td');
+
+                tableCell.innerHTML =
+                    `<p class="cell__content 
+                        ${field === "medical_indicators" && "document" ||
+                          field === "photo" && "eye" ||
+                          field === "actions" && "edit"
+                        }">
+                    ${field === 'actions' ? `<div class="delete"></div>` : item[field]}
+                    </p>`;
+                tableCell.classList.add('cell');
+                tableRow.appendChild(tableCell);
+            }
+
+            tableContent.appendChild(tableRow);
+        });
+
+        count++;
+    }
 });
